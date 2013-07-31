6c475167e143ef9e8b023e1de7daac8fc3f8c56f
Berhomus <borhane.bensaid@laposte.net>
2013-07-31 11:14:20 +0200

    bdd

--- backup_bdd/plugit2.sql
+++ backup_bdd/plugit2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le : Jeu 25 Juillet 2013 à 09:35
--- Version du serveur: 5.5.20
--- Version de PHP: 5.3.10
+-- Généré le: Mer 31 Juillet 2013 à 09:13
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.3.13
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `plugit`
+-- Base de données: `plugit2`
 --
 
 -- --------------------------------------------------------
@@ -31,14 +31,15 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `login` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `mdp_md5` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `admin`
 --
 
 INSERT INTO `admin` (`id`, `login`, `mdp_md5`) VALUES
-(2, 'plugit', 'b04942b84582fc7f84712a538b7b8829');
+(2, 'plugit', 'b04942b84582fc7f84712a538b7b8829'),
+(3, 'Boby', '2a0e48c59136f164b54e2eeb029e5ebb');
 
 -- --------------------------------------------------------
 
@@ -78,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `lien` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `meta` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=14 ;
 
 --
 -- Contenu de la table `menu`
@@ -86,12 +87,17 @@ CREATE TABLE IF NOT EXISTS `menu` (
 
 INSERT INTO `menu` (`id`, `interne`, `active`, `nom`, `baseName`, `position`, `lien`, `meta`) VALUES
 (1, 1, 1, 'Accueil', 'accueil', 1, 'Index.php?page=accueil&sub=main', 'Société de services en informatique spécialisée dans l’infogérance, l’hébergement de systèmes d’informations la mise en place de solutions de cloud computing et la maintenance de réseaux informatique.'),
-(2, 1, 1, 'Solutions', 'solutions', 2, 'Index.php?page=solutions&mode=view', 'Les solutions de plug-it sont : WanaDesk vous permet d’accéder à votre bureau virtuel depuis n’importe où* sur la planète !\r\n*connexion Internet requise. WanaDev vous propose une solution logicielle de gestion commerciale « en ligne » et entièrement sur-mesure... WanaTel vous offre la téléphonie HD, moderne, aux fonctionnalités étendues, en réduisant au maximum vos coûts ! WanaMail vous permet d’accéder à votre messagerie en tout lieu et de partager l’ensemble de vos fonctionnalités entre tous vos salariés ! WanaStore vous offre une sauvegarde automatique et sécurisée à 100 % de toutes vos données, en toute sérénité.. WanaBox vous permet de stocker tous vos documents sur l''ensemble de vos ordinateurs, tablettes et smartphones !'),
-(3, 1, 1, 'Références', 'references', 3, 'Index.php?page=references&mode=view', 'Les principaux clients de Plug-it, satisfait des solutions proposées et de la mise en place de nos services d''infogérance, cloud computing, maintenance réseaux ...'),
-(4, 1, 1, 'Contact', 'contact', 4, 'Index.php?page=contact', 'Contactez plug-it à Amiens pour tout renseignement sur nos solutions informatiques, de cloud computing, d''infogérance, d''hébergement de SI et de maintenance de réseaux'),
-(5, 1, 1, 'Support', 'support', 5, 'Index.php?page=support', 'Un problème en informatique, plug-it propose à ses clients une assistance téléphonique afin de résoudre vos problèmes le plus rapidement et efficacement que possible.'),
+(2, 1, 0, 'Solutions', 'solutions', 2, 'Index.php?page=solutions&mode=view', 'Les solutions de plug-it sont : WanaDesk vous permet d’accéder à votre bureau virtuel depuis n’importe où* sur la planète !\r\n*connexion Internet requise. WanaDev vous propose une solution logicielle de gestion commerciale « en ligne » et entièrement sur-mesure... WanaTel vous offre la téléphonie HD, moderne, aux fonctionnalités étendues, en réduisant au maximum vos coûts ! WanaMail vous permet d’accéder à votre messagerie en tout lieu et de partager l’ensemble de vos fonctionnalités entre tous vos salariés ! WanaStore vous offre une sauvegarde automatique et sécurisée à 100 % de toutes vos données, en toute sérénité.. WanaBox vous permet de stocker tous vos documents sur l''ensemble de vos ordinateurs, tablettes et smartphones !'),
+(3, 1, 0, 'Références', 'references', 3, 'Index.php?page=references&mode=view', 'Les principaux clients de Plug-it, satisfait des solutions proposées et de la mise en place de nos services d''infogérance, cloud computing, maintenance réseaux ...'),
+(4, 1, 0, 'Contact', 'contact', 4, 'Index.php?page=contact', 'Contactez plug-it à Amiens pour tout renseignement sur nos solutions informatiques, de cloud computing, d''infogérance, d''hébergement de SI et de maintenance de réseaux'),
+(5, 1, 1, 'Support', 'support', 8, 'Index.php?page=support', 'Un problème en informatique, plug-it propose à ses clients une assistance téléphonique afin de résoudre vos problèmes le plus rapidement et efficacement que possible.'),
 (6, 1, 1, 'Paiement', 'reglement', 6, 'Index.php?page=reglement', 'Vous êtes client de plug-it et vous souhaitez régler vos factures, cette page vous permet de payer en ligne en toute sécurité.'),
-(7, 1, 0, 'Boutique', 'boutique', 6, 'Index.php?page=boutique', 'Vous êtes client de plug-it et vous souhaitez régler vos factures, cette page vous permet de payer en ligne en toute sécurité.');
+(7, 1, 1, 'Boutique', 'boutique', 7, 'Index.php?page=boutique', 'Vous êtes client de plug-it et vous souhaitez régler vos factures, cette page vous permet de payer en ligne en toute sécurité.'),
+(9, 1, 1, 'Services', 'services', 4, 'Index.php?page=services&mode=view', 'Découvrez les services innovants de Plug-it, comme le cloud computing'),
+(10, 1, 1, 'CloudPrivé', 'cloud', 2, 'Index.php?page=cloud', ''),
+(11, 1, 1, 'WebApps', 'webapps', 3, 'Index.php?page=webapps', ''),
+(12, 1, 1, 'Téléphonie', 'telephonie', 5, 'Index.php?page=telephonie', ''),
+(13, 1, 1, 'Photocopieur', 'photocopieur', 9, '', '');
 
 -- --------------------------------------------------------
 
@@ -109,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `categorie` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `priorite` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -191,7 +197,7 @@ CREATE TABLE IF NOT EXISTS `solutions` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ordre` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
 
 --
 -- Contenu de la table `solutions`
@@ -205,6 +211,38 @@ INSERT INTO `solutions` (`id`, `titre`, `corps`, `image_car`, `image_sol`, `desc
 (5, 'WanaStore', '<p>\r\n<img style="margin-right:10px;" src="images/fleche.png"/><span class="titre">Sauvegardez vos données stratégiques. Protéger les données qui garantissent la bonne marche de votre entreprise.</span>\r\n\r\nPlug-it propose une télé sauvegarde incrémentielle à l’octet via une simple ligne internet.\r\nAprès la sauvegarde initiale, Plug-it sauvegarde uniquement les morceaux de fichier nouveaux ou modifiés, économisant ainsi la bande passante et garantissant des sauvegardes ultérieures extrêmement rapides.\r\n</p>\r\n<p>\r\n<img style="margin-right:10px;" src="images/fleche.png"/><span class="titre">À partir de 1 Go, nombre de poste illimité et coût maitrisé</span>\r\n</p>\r\n', 'images/slide_05.jpg', 'images/solutions_wanastore.jpg', 'vous offre une sauvegarde automatique et sécurisée à 100 % de toutes vos données, en toute sérénité...', '2013-06-21 09:08:51', 9),
 (6, 'WanaBox', '<p>\r\n<img style="margin-right:10px;" src="images/fleche.png"/><span class="titre">Stocker l''ensemble de vos documents</span>\r\n\r\n- Disponible sur l''ensemble de vos ordinateurs.\r\n- Accessible sur toutes vos tablettes et smartphones.\r\n- Sauvegardes quotidiennes disponibles sur 45 jours.\r\n- Partagez vos données avec d''autres utilisateurs.\r\n- Travaillez en mode déconnecté sur vos documents.\r\n</p>\r\n<p>\r\n<img style="margin-right:10px;" src="images/fleche.png"/><span class="titre">Des tarifs très attractifs selon chaque utilisation</span>\r\n<b>\r\n- Stockage jusqu''à 20 Go : 4,90 € HT par mois.\r\n- Stockage de 20 Go à 49 Go : 6,90 € HT par mois.\r\n- Stockage de 49 Go à 100 Go : 14,90 € HT par mois.\r\n</b>\r\n</p>\r\n', 'images/slide_06.jpg', 'images/solutions_wanabox.jpg', 'vous permet de stocker tous vos documents sur l''ensemble de vos ordinateurs, tablettes et smartphones !', '2013-06-21 09:13:55', 9),
 (7, 'WanaShare', '<p>\r\n<img style="margin-right:10px;" src="images/fleche.png"/><span class="titre">WanaShare est une plateforme de gestion de l''information et de collaboration professionnelle basée sur le produit Microsoft© SharePoint 2010 qui vous aide à améliorer votre productivité et à gérer votre contenu, \r\nen utilisant un navigateur Internet.</span>\r\n\r\nLes fonctionnalités intégrées de WanaShare, optimisées par des technologies d''indexation et de \r\nrecherche, vous permettent de vous adapter rapidement à l''évolution de vos besoins métier.\r\nVous pouvez ainsi prendre des décisions fondées sur des données métier consolidées et déployer des \r\napplications métiers de façon rapide et sécurisée afin de renforcer la collaboration dans et hors de votre\r\nentreprise.\r\n</p>\r\n<p>\r\n<img style="margin-right:10px;" src="images/fleche.png"/><span class="titre">WanaShare, pour quels usages ? WanaShare vous permet d''accroître la productivité via un ensemble intégré de\r\nfonctionnalités innovantes. Parmi elles, on peut citer :</span>\r\n\r\n- Accessibilité.\r\n- Affichage d''informations.\r\n- Prise en main de WSS 3.0.\r\n- Conservation de plusieurs versions de fichiers et d''éléments.\r\n- Création de sites, de listes et de bibliothèques.\r\n- Formules et fonctions.\r\n- Gestion de sites et de paramètres.\r\n- Intégration du courrier électronique aux sites, listes et bibliothèques.\r\n- Organisation de réunions.\r\n- Partage de fichiers et de documents.\r\n- Partage d''informations.\r\n- Personnalisation de sites, de pages, de listes et de bibliothèques.\r\n- Utilisation des environnements internationaux.\r\n- Utilisation des flux de travail pour gérer les processus.\r\n</p>', 'images/slide_07.jpg', 'images/solutions_wanashare.jpg', 'vous permet de gérer l’information en collaboration professionnelle, en utilisant juste un navigateur Internet.', '2013-06-21 09:19:48', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sousmenu`
+--
+
+CREATE TABLE IF NOT EXISTS `sousmenu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `lien` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `position` int(11) NOT NULL,
+  `menu` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
+
+--
+-- Contenu de la table `sousmenu`
+--
+
+INSERT INTO `sousmenu` (`id`, `nom`, `active`, `lien`, `position`, `menu`) VALUES
+(1, 'Skydrive', 1, 'skydrive', 1, 11),
+(2, 'Lync', 1, 'lync', 2, 11),
+(3, 'Office', 1, 'office', 3, 11),
+(4, 'Outlook', 1, 'outlook', 4, 11),
+(5, 'Infogérance', 1, 'index.php?page=services&mode=viewone&id=3', 1, 9),
+(6, 'Audit & Conseil', 1, 'index.php?page=services&mode=viewone&id=4', 2, 9),
+(7, 'Maintenance', 1, 'index.php?page=services&mode=viewone&id=2', 3, 9),
+(8, 'Architecture réseau', 1, 'index.php?page=services&mode=viewone&id=5', 4, 9),
+(9, 'Virtualisation', 1, 'index.php?page=services&mode=viewone&id=6', 5, 9),
+(10, 'Mise à la norme des PCA', 1, 'index.php?page=services&mode=viewone&id=7', 6, 9);
 
 -- --------------------------------------------------------
 
