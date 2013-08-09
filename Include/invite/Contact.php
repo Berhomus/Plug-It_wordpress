f62271dfca2025c104e03667e56f1bb340ca2bf2
Berhomus <borhane.bensaid@laposte.net>
2013-08-09 16:55:34 +0200

    deb

--- Include/invite/Contact.php
+++ Include/invite/Contact.php
@@ -4,9 +4,28 @@ Last Update : 12/07/2013
 Name : Contact.php => Plug-it
 *********************************************************-->
 
+<?php
+	require_once('./connexionbddplugit.class.php');
+	$bdd=connexionbddplugit::getInstance();  
+?>
+
 <script type="text/javascript" src="js/fct_de_trt_txt.js"></script>
 
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<link rel="stylesheet" href="./styles/contact.css" />
+
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
 <?php
+	$rq2 = $bdd->prepare("SELECT COUNT(id) AS nbre FROM contact");
+	$rq2->execute(array());
+
+	$ar2 = $rq2->fetch();
+
+	$rq = $bdd->prepare("SELECT * FROM contact");
+	$rq->execute(array());
+
 
 	$error_contact = 0;
 	if(isset($_POST) && !empty($_POST))
@@ -16,29 +35,27 @@ Name : Contact.php => Plug-it
 			$message = "";
 			$societe = (isset($_POST['societe'])) ? $_POST['societe']:"";
 			$objet = (isset($_POST['objet'])) ? $_POST['objet']:"";
-			
+
 			$message = $_POST['liste'] ." ". $_POST['nom'] ." ". $_POST['prenom'] ."\n".$societe."-".$_POST['courriel']."\n".$_POST['message'];
-			
-			 if(mail('contact@plug-it.com', $objet,$message))
-			 {
+
+			if(mail($_POST['courriel'], $objet,$message))
+			{
 				$error_contact = 2;
-			 }
-			 else
-			 {
+			}
+			else
+			{
 				$error_contact = 3;
-			 }
-		
+			}
+
 			$error_contact = 2;
 		}
 		else
 			$error_contact = 1;
 	}
-?>
 
-<?php
-	echo '<div id="tabs">';
-			echo '<ul>';
-	
+	echo '<div id="tabs">
+	<ul>'; 
+
 	$i=1;	
 	while($ar = $rq->fetch())
 	{
@@ -47,80 +64,77 @@ Name : Contact.php => Plug-it
 	}
 	echo '</ul>';
 	$i=1;
-	
+
 	$rq = $bdd->prepare("SELECT * FROM contact");
 	$rq->execute(array());
-	
+
 	while($ar = $rq->fetch())
 	{
-	echo '
+		echo '
 			<div id="tabs-'.$i.'">
-				<input type="hidden" id="mail" name="mail" value="'.$ar['courriel'].'"/>
-				<input type="hidden" id="lat" name="lat" value="'.$ar['latitude'].'"/>
-				<input type="hidden" id="longi" name="longi" value="'.$ar['longitude'].'"/>';
+			<input type="hidden" id="mail" name="mail" value="'.$ar['courriel'].'"/>
+			<input type="hidden" id="lat" name="lat" value="'.$ar['latitude'].'"/>
+			<input type="hidden" id="longi" name="longi" value="'.$ar['longitude'].'"/>';
 
-				?>
+?>
 
-				<div style="overflow:hidden;">
+<div style="overflow:hidden;">
 
-					<div style="width:62%;float:left;">
-					<?php
-						if(isset($_SESSION['id']))
-								{
-									echo'
-									<span style="margin-left:40px;"><a class="bt" href="'.$_SESSION['protocol'].$_SESSION['current_loc'].'index.php?page=admin_contact&mode=modifier&id='.$ar['id'].'">Modifier</a> - 
-									<a class="bt" href="'.$_SESSION['protocol'].$_SESSION['current_loc'].'traitement/trt_contact.php?mode=delete&id='.$ar['id'].'">Supprimer</a></span>';
-								}
-					?>
+	<div style="float:left;">
+		<?php
+		if(isset($_SESSION['id']))
+		{
+			echo'
+			<span style="margin-left:40px;"><a class="bt" href="'.$_SESSION['protocol'].$_SESSION['current_loc'].'index.php?page=admin_contact&mode=modifier&id='.$ar['id'].'">Modifier</a> - 
+			<a class="bt" href="'.$_SESSION['protocol'].$_SESSION['current_loc'].'traitement/trt_contact.php?mode=delete&id='.$ar['id'].'">Supprimer</a></span>';
+		}
+		?>
+		<div style="float:left;">
 
-						<h2 class="titre" style="margin-top:40px;">Contactez-nous</h2>
+		<h2 class="titre">Contactez-nous</h2>
 
-<div style="width:60%;float:left;">
-
-<h2 class="titre">Contactez-nous</h2>
-
-<form method="post" action="#">
-	<table border="0" cellspacing="20" cellpadding="5" style="margin:auto; margin-right:150px;">
+		<form method="post" action="#">
+			<table border="0" cellspacing="20" cellpadding="5" style="margin:auto; margin-right:150px;">
 				<tr>
 					<td><b>Civilité <span class="red">*</span></b></td>
 					<td><select name="liste" required>
-						<option></option>
-						<option value="M">M.</option>
-						<option value="Mme">Mme</option>
+							<option></option>
+							<option value="M">M.</option>
+							<option value="Mme">Mme</option>
 						</select>
 					</td>
 				</tr>
-				
+
 				<tr>
 					<td><label for="nom"><b>Nom <span class="red">*</span></b></label></td>
 					<td><input type="text" name="nom" id="nom" required/></td>
 				</tr>
-				
+
 				<tr>
 					<td><label for="prenom"><b>Prénom <span class="red">*</span></b></label></td>
 					<td><input type="text" name="prenom" id="prenom" required/></td>
 				</tr>
-				
+
 				<tr>
 					<td><label for="societe"><b>Société </b></label></td>
 					<td><input type="text" name="societe" id="societe" /></td>
 				</tr>
-				
+
 				<tr>
 					<td><label for="courriel" id="email"><b>Courriel <span class="red">*</span></b></label></td>
 					<td><input type="text" name="courriel" id="courriel" onblur="isEmail(this,email);" required/></td>
 				</tr>
-				
+
 				<tr>	
 					<td><label for="objet"><b>Objet </b></label></td>
 					<td><input type="text" name="objet" id="objet" /></td>
 				</tr>
-				
+
 				<tr>
 					<td><label for="message"><b>Message <span class="red">*</span></b></label></td>
 					<td><textarea name="message" id="message" rows="15" cols="40" style="resize:none;" required></textarea></td>
 				</tr>
-				
+
 				<tr>
 					<td>
 					<?php
@@ -131,9 +145,11 @@ Name : Contact.php => Plug-it
 								case 1:
 									echo '<h2 style="color:red;">Email invalide !</h2>';
 								break;
+								
 								case 2:
 									echo '<h2 style="color:green;">Message Envoyé !</h2>';
 								break;
+
 								case 3:
 									echo '<h2 style="color:red;">Message Non-Envoyé !</h2>';
 								break;
@@ -143,39 +159,71 @@ Name : Contact.php => Plug-it
 					</td>
 					<td style="text-align:right;"><input type="submit" name="envoyer" value="Envoyer" /></td>
 				</tr>		
-		</table>
-	</form>	
+			</table>
+		</form>	
+		</div>
+
+		<div style="float:left;">
+			<hr class="separation"/>
+		</div>
+		<?php
+			echo '<div style="float:left; margin-top:40px; margin-left:40px">
+				<h2>Notre agence</h2>
+				<br/>';
+			echo nl2br($ar['coordonnees']);  
+			echo '<br/>
+				<a class="mail" href="mailto:'.$ar['courriel'].'">'.$ar['courriel'].'</a>
+				<br/><br/>';
+
+			/*Carte google API*/
+
+			echo '<div id="div_carte"></div>';
+			echo '</div>';
+		?>
+
+	</div>
+	<?php
+	echo '</div></div>';
+	$i++;
+	}
+	?>
+</div>
+<div style="padding-bottom:40px;">
 </div>
 
-<div style="float:left;">
-<hr class="separation" />
-</div>
+<script type="text/javascript">
+	var lat = document.getElementById('lat').value;
+	var longi = document.getElementById('longi').value;
 
-<div style="float:left; margin-left:2%;">
-	<h2>Notre agence</h2>
-	<br/>
-	<p><b>
-		Plug-It
-		<br/>
-		36 bis, rue Saint-Fuscien
-		<br/>
-		80000 Amiens
-	
-	</b></p>
-	<br/>
-	<p>
-	Tél. : 03 22 22 10 90
-	<br/>
-	Fax : 03 22 80 76 52
-	<br/>
-	<a class="mail" href="mailto:contact@plug-it.com">contact@plug-it.com</a>
-	</p>
-	<br/>
-	
-	<!--Carte google API-->
-			
-	<div id="div_carte"></div>		
-			
-</div>
+	var pos = new google.maps.LatLng(lat, longi);
+	function initCarte()
+	{
+		// création de la carte
+		var oMap = new google.maps.Map( document.getElementById( 'div_carte'),
+		{
+		'center' : pos,
+		'zoom' : 17,
+		'mapTypeId' : google.maps.MapTypeId.ROADMAP
+		});
 
-</div>
+		var myMarker = new google.maps.Marker({
+		// Coordonnées du site
+		position: pos,
+		map: oMap,
+		title: "Plug-it"
+		});
+
+		var myWindowOptions = {
+		content:
+		'<h6>Plug-it</h6>'
+		};
+		var myInfoWindow = new google.maps.InfoWindow(myWindowOptions);
+	}
+	// init lorsque la page est chargée
+	google.maps.event.addDomListener( window, 'load', initCarte);
+
+	/*####ONGLETS####*/
+	$(function() {
+	$( "#tabs" ).tabs();
+	});
+</script>
