1ae95d287e3534216b6ce9d1b8c3c043e2702c34
Berhomus <borhane.bensaid@laposte.net>
2013-08-16 09:13:59 +0200

    contact fini

--- Include/invite/Contact.php
+++ Include/invite/Contact.php
@@ -16,39 +16,13 @@ Name : Contact.php => Plug-it
 <link rel="stylesheet" href="./styles/contact.css" />
 
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
-<script>
-function getXMLHttpRequest() {
-	var xhr = null;
-	 
-	if (window.XMLHttpRequest || window.ActiveXObject) {
-		if (window.ActiveXObject) {
-			try {
-				xhr = new ActiveXObject("Msxml2.XMLHTTP");
-			} catch(e) {
-				xhr = new ActiveXObject("Microsoft.XMLHTTP");
-			}
-		} else {
-			xhr = new XMLHttpRequest();
-		}
-	} else {
-		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
-		return null;
-	}
-	 
-	return xhr;
-}
-
-
-</script>
 
 <?php
-	$rq2 = $bdd->prepare("SELECT COUNT(id) AS nbre FROM contact");
-	$rq2->execute(array());
-
-	$ar2 = $rq2->fetch();
-
-	$rq = $bdd->prepare("SELECT * FROM contact");
-	$rq->execute(array());
+	if(!isset($_GET['ville']))
+		$_GET['ville'] = 'AMIENS';
+		
+	$rq = $bdd->prepare("SELECT * FROM contact WHERE upper(ville)=?");
+	$rq->execute(array($_GET['ville']));
 
 
 	$error_contact = 0;
@@ -77,30 +51,23 @@ function getXMLHttpRequest() {
 			$error_contact = 1;
 	}
 
-	echo '<div id="tabs">
-	<ul>'; 
 
-	$i=0;	
-	while($ar = $rq->fetch())
-	{
-		echo '<li><a href="#tabs-'.$i.'">'.$ar['ville'].'</a></li>';
-		$i++;
-	}
-	echo '</ul>';
-	$i=0;
-
-	$rq = $bdd->prepare("SELECT * FROM contact");
-	$rq->execute(array());
 	
-	while($ar = $rq->fetch())
+	if($rq->rowCount() == 1)
 	{
+	
+		$rq2 = $bdd->prepare("SELECT * FROM contact");
+		$rq2->execute(array());
+		while($ar2 = $rq2->fetch())
+			echo '<a class="mail" href="index.php?page=contact&ville='.strtoupper($ar2['ville']).'"><span class="style" style="display:inline;">'.$ar2['ville'].'</span></a>';
+		
+		$ar = $rq->fetch();
+		
 		echo '
-			<div id="tabs-'.$i.'">
 			<input type="hidden" id="mail" name="mail" value="'.$ar['courriel'].'"/>
-
-			<input type="hidden" id="lat_'.$i.'" name="lat" value="'.$ar['latitude'].'"/>
-			<input type="hidden" id="longi_'.$i.'" name="longi" value="'.$ar['longitude'].'"/>';
-			
+			<input type="hidden" id="lat" name="lat" value="'.$ar['latitude'].'"/>
+			<input type="hidden" id="longi" name="longi" value="'.$ar['longitude'].'"/>';
+				
 ?>
 
 <div style="overflow:hidden;">
@@ -114,7 +81,7 @@ function getXMLHttpRequest() {
 			<a class="bt" href="'.$_SESSION['protocol'].$_SESSION['current_loc'].'traitement/trt_contact.php?mode=delete&id='.$ar['id'].'">Supprimer</a></span>';
 		}
 		?>
-		<div style="float:left;">
+		<div style="float:left; margin-top:20px;">
 
 		<h2 class="titre">Contactez-nous</h2>
 
@@ -201,49 +168,33 @@ function getXMLHttpRequest() {
 				<br/><br/>';
 
 			/*Carte google API*/
-			echo "<div id='carte".$i."' style='width:300px;height:300px;border:2px black solid;'></div>";
+			echo "<div id='carte' style='width:300px;height:300px;border:2px black solid;'></div>";
 			
 			echo '</div>';
 		?>
 	</div>
-	<?php
-	echo '</div></div>';
-	$i++;
-	}
-	echo '<input type="hidden" value="'.($i-1).'" id="nbr_ville"/>';
-	?>
+
 </div>
+<?php
+}
+else
+{
+	echo 'Ville Inexistante';
+}
+?>
 <div style="padding-bottom:40px;">
 </div>
 
 <script type="text/javascript">
 
-// function loadmap(){
-	// xhr = getXMLHttpRequest();
-		
-		// xhr.onreadystatechange = function() {
-			// if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-
-				// document.getElementById("lolilol").innerHTML = xhr.responseText();
-			// }
-			// else
-			// {
-				// document.getElementById("lolilol").innerHTML = '<img src="img/loader.gif"/>';
-			// }
-		// };
-
-		// xhr.open("GET", "https://maps.google.fr/maps?f=q&hl=fr&q=49.02685,2.015&amp;z=17&amp;output=embed", true);
-// }
-
-
- function initCarte(i)
+ function initCarte()
  {
   // création de la carte
 	var lat = document.getElementById('lat').value;
-	var longi = document.getElementById('long').value;
+	var longi = document.getElementById('longi').value;
 	var pos = new google.maps.LatLng(lat, longi);
   
-	  var oMap = new google.maps.Map( document.getElementById("carte"+i),
+	  var oMap = new google.maps.Map( document.getElementById("carte"),
 	  {
 	  'center' : pos,
 	  'zoom' : 17,
@@ -266,12 +217,4 @@ function getXMLHttpRequest() {
  // init lorsque la page est chargée
 
 google.maps.event.addDomListener( window, 'load', initCarte);
-
-
-
-
-	/*####ONGLETS####*/
-	$(function() {
-	$( "#tabs" ).tabs();
-	});
 </script>
