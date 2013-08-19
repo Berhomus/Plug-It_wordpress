3eedcfe85f36f206118277ffb229671c54354996
Berhomus <borhane.bensaid@laposte.net>
2013-08-19 09:50:03 +0200

    design

--- Include/invite/Contact.php
+++ Include/invite/Contact.php
@@ -24,7 +24,6 @@ Name : Contact.php => Plug-it
 	$rq = $bdd->prepare("SELECT * FROM contact WHERE upper(ville)=?");
 	$rq->execute(array($_GET['ville']));
 
-
 	$error_contact = 0;
 	if(isset($_POST) && !empty($_POST))
 	{
@@ -50,8 +49,6 @@ Name : Contact.php => Plug-it
 		else
 			$error_contact = 1;
 	}
-
-
 	
 	if($rq->rowCount() == 1)
 	{
@@ -83,76 +80,76 @@ Name : Contact.php => Plug-it
 		?>
 		<div style="float:left; margin-top:20px;">
 
-		<h2 class="titre">Contactez-nous</h2>
+			<h2 class="titre">Contactez-nous</h2>
 
-		<form method="post" action="#">
-			<table border="0" cellspacing="20" cellpadding="5" style="margin:auto; margin-right:150px;">
-				<tr style="height:40px;">
-					<td><b>Civilité <span class="red">*</span></b></td>
-					<td><select name="liste" required>
-							<option></option>
-							<option value="M">M.</option>
-							<option value="Mme">Mme</option>
-						</select>
-					</td>
-				</tr>
+			<form method="post" action="#">
+				<table border="0" cellspacing="20" cellpadding="5" style="margin:auto; margin-right:150px;">
+					<tr style="height:40px;">
+						<td><b>Civilité <span class="red">*</span></b></td>
+						<td><select name="liste" required>
+								<option></option>
+								<option value="M">M.</option>
+								<option value="Mme">Mme</option>
+							</select>
+						</td>
+					</tr>
 
-				<tr style="height:40px;">
-					<td><label for="nom"><b>Nom <span class="red">*</span></b></label></td>
-					<td><input type="text" name="nom" id="nom" required/></td>
-				</tr>
+					<tr style="height:40px;">
+						<td><label for="nom"><b>Nom <span class="red">*</span></b></label></td>
+						<td><input type="text" name="nom" id="nom" required/></td>
+					</tr>
 
-				<tr style="height:40px;">
-					<td><label for="prenom"><b>Prénom <span class="red">*</span></b></label></td>
-					<td><input type="text" name="prenom" id="prenom" required/></td>
-				</tr>
+					<tr style="height:40px;">
+						<td><label for="prenom"><b>Prénom <span class="red">*</span></b></label></td>
+						<td><input type="text" name="prenom" id="prenom" required/></td>
+					</tr>
 
-				<tr style="height:40px;">
-					<td><label for="societe"><b>Société </b></label></td>
-					<td><input type="text" name="societe" id="societe" /></td>
-				</tr>
+					<tr style="height:40px;">
+						<td><label for="societe"><b>Société </b></label></td>
+						<td><input type="text" name="societe" id="societe" /></td>
+					</tr>
 
-				<tr style="height:40px;">
-					<td><label for="courriel" id="email"><b>Courriel <span class="red">*</span></b></label></td>
-					<td><input type="text" name="courriel" id="courriel" onblur="isEmail(this,email);" required/></td>
-				</tr>
+					<tr style="height:40px;">
+						<td><label for="courriel" id="email"><b>Courriel <span class="red">*</span></b></label></td>
+						<td><input type="text" name="courriel" id="courriel" onblur="isEmail(this,email);" required/></td>
+					</tr>
 
-				<tr style="height:40px;">	
-					<td><label for="objet"><b>Objet </b></label></td>
-					<td><input type="text" name="objet" id="objet" /></td>
-				</tr>
+					<tr style="height:40px;">	
+						<td><label for="objet"><b>Objet </b></label></td>
+						<td><input type="text" name="objet" id="objet" /></td>
+					</tr>
 
-				<tr>
-					<td><label for="message"><b>Message <span class="red">*</span></b></label></td>
-					<td><textarea name="message" id="message" rows="15" cols="40" style="resize:none;" required></textarea></td>
-				</tr>
+					<tr>
+						<td><label for="message"><b>Message <span class="red">*</span></b></label></td>
+						<td><textarea name="message" id="message" rows="15" cols="40" style="resize:none;" required></textarea></td>
+					</tr>
 
-				<tr style="height:40px;">
-					<td>
-					<?php
-						if($error_contact != 0)
-						{
-							switch($error_contact)
+					<tr style="height:40px;">
+						<td>
+						<?php
+							if($error_contact != 0)
 							{
-								case 1:
-									echo '<h2 style="color:red;">Email invalide !</h2>';
-								break;
-								
-								case 2:
-									echo '<h2 style="color:green;">Message Envoyé !</h2>';
-								break;
+								switch($error_contact)
+								{
+									case 1:
+										echo '<h2 style="color:red;">Email invalide !</h2>';
+									break;
+									
+									case 2:
+										echo '<h2 style="color:green;">Message Envoyé !</h2>';
+									break;
 
-								case 3:
-									echo '<h2 style="color:red;">Message Non-Envoyé !</h2>';
-								break;
-							}	
-						}
-					?>
-					</td>
-					<td style="text-align:right;"><input type="submit" name="envoyer" value="Envoyer" /></td>
-				</tr>		
-			</table>
-		</form>	
+									case 3:
+										echo '<h2 style="color:red;">Message Non-Envoyé !</h2>';
+									break;
+								}	
+							}
+						?>
+						</td>
+						<td style="text-align:right;"><input type="submit" name="envoyer" value="Envoyer" /></td>
+					</tr>		
+				</table>
+			</form>	
 		</div>
 
 		<div style="float:left;">
